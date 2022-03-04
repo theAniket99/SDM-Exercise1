ce78de2cd50108ec7d529aaec2f3627842172301
theAniket99 <94819907+theAniket99@users.noreply.github.com>
2022-03-04 17:05:00 +0530

    Update App.java

--- src/main/java/labexam/exercise1/App.java
+++ src/main/java/labexam/exercise1/App.java
@@ -1,1 +1,45 @@
 
+package labexam.exercise1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App 
+{
+	WebDriver drive;
+	
+	@SuppressWarnings("deprecation")
+	public void runBrowser() {
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		drive = new ChromeDriver();
+		
+		drive.manage().window().maximize();
+		drive.manage().deleteAllCookies();
+		
+		drive.manage().timeouts().pageLoadTimeout(80, TimeUnit.SECONDS);
+		drive.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+
+		drive.get("http://www.flipkart.com");
+	}
+	
+	public void loginToFlipkart() {
+		drive.findElement(By.xpath("//input[@class='_2IX_2- VJZDxU']")).sendKeys("shindeAniket@abc.com");
+		
+		drive.findElement(By.xpath("//input[@class='_2IX_2- _3mctLh VJZDxU']")).sendKeys("Aniket@1234");
+		
+		drive.findElement(By.xpath("//button[@class='_2KpZ6l _2HKlqd _3AWRsL']")).click();
+		
+		//sir it's working on xpath but login will fail here cause of wrong password & username
+	}
+	
+    public static void main( String[] args )
+    {
+    	App obj = new App();
+    	
+    	obj.runBrowser();
+    	obj.loginToFlipkart();
+    }
+}
